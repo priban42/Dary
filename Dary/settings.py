d9204df73701c4ddf4c9bafb31a3b516537d48a7
priban42 <vojtech.priban@student.gyarab.cz>
2020-05-26 15:41:17 +0200

    heroku shit

--- Dary/settings.py
+++ Dary/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+DISABLE_COLLECTSTATIC=1
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
